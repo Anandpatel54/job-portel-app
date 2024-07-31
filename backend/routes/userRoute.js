a82068241af0c1a0e35f5459c465dfa2c35f66b3
Anand patel <panand054@gmail.com>
2024-07-31 11:47:57 +0530

    multer part completed

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -9,10 +9,10 @@ import isAuthenticated from "../middlewares/isAuthenticated.js";
 import { singleUpload } from "../middlewares/multer.js";
 const router = express.Router();
 
-router.post("/register",singleUpload, register);
+router.post("/register", singleUpload, register);
 //router.route("/register").post(register)
 router.post("/login", login);
 router.get("/logout", logout);
-router.post("/Profile/update", isAuthenticated, updateProfile);
+router.post("/Profile/update", isAuthenticated, singleUpload, updateProfile);
 
 export default router;
