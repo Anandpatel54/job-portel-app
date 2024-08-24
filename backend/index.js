8e0341734af23b1d26849064b96429d2a8939ecf
Anand patel <panand054@gmail.com>
2024-08-24 10:31:32 +0530

    done

--- backend/index.js
+++ backend/index.js
@@ -27,6 +27,10 @@ app.use("/api/v1/company", companyRoute);
 app.use("/api/v1/job", jobRoute);
 app.use("/api/v1/application", applicationRoute);
 
+app.get("/", (req, res) => {
+  res.send("API working");
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   connectDB();
