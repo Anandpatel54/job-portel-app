ed64ee57505a67f5fdebbc044b9b47af4cffc325
Anand patel <panand054@gmail.com>
2024-09-14 16:55:38 +0530

    All part done

--- backend/index.js
+++ backend/index.js
@@ -34,7 +34,7 @@ app.use("/api/v1/job", jobRoute);
 app.use("/api/v1/application", applicationRoute);
 
 app.use(express.static(path.join(__dirname, "/frontend/dist")));
-app.get("*", (req, res) => {
+app.get("*", (_, res) => {
   res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
 });
 
